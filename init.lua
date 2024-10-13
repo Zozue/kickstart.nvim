e58b50edf783e4bb50d7798723b0aaeb898f6c47
Joshua Loke <32748817+Zozue@users.noreply.github.com>
2024-10-13 15:04:21 -0400

    Enable telescope colorpicker previews

--- init.lua
+++ init.lua
@@ -388,6 +388,11 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        pickers = {
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
